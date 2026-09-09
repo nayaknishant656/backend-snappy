b925ba71ee3cc8d0157b7df08237f4339935bf52
nayaknishant656 <nayaknishant656@gmail.com>
2026-09-09 20:51:23 +0530

    developed the routes for the connectionjs along with the model of the connection.js

--- src/routes/connection.js
+++ src/routes/connection.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllConnections, getConnectionmmap } from '../controllers/connection.js';
+import { getAllConnections, getConnectionmmap, getConnblog } from '../controllers/connection.js';
 
 const router = express.Router();
 
 router.get('/conn/:id', getAllConnections);
 router.get('/connmap', getConnectionmmap);
+router.get('/connblog/:id', getConnblog);
 
 
 export default router;
