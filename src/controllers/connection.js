02625f9c3d074115ed536a6c21a62fa0d7769e6b
nayaknishant656 <nayaknishant656@gmail.com>
2026-09-09 20:50:28 +0530

    developed the Mongodb Schema Model for the Blogpost

--- src/controllers/connection.js
+++ src/controllers/connection.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 import Connection from '../models/connection.js';
+import Blog from '../models/connection.js';
 import Page from ".././Components/page.js";
 import { renderToStaticMarkup } from "react-dom/server";
 import Connectionmap from "../Components/Connectionmap.js";
@@ -46,6 +47,37 @@ export const getConnectionmmap = async (req, res) => {
             .status(200)
             .type("html")
             .send(html);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: "Failed to generate HTML page.",
+            error: error.message,
+        });
+    }
+};
+
+export const getConnblog = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send("<h2>Invalid Product ID</h2>");
+        }
+
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.status(404).send("<h2>Product Not Found</h2>");
+        }
+        return res
+            .status(200)
+            .json({
+                success: true,
+                blog,
+            });
+
+
 
     } catch (error) {
 
